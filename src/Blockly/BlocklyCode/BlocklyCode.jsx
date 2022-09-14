cf74ff98cf4a86547454f8c12c57af80241fdd77
intern-desktop <justin.c.kirk@gmail.com>
2022-09-14 13:27:13 -0400

    adds download button for code

--- src/Blockly/BlocklyCode/BlocklyCode.jsx
+++ src/Blockly/BlocklyCode/BlocklyCode.jsx
@@ -1,12 +1,19 @@
  import React, {useState} from 'react';
  import './BlocklyCode.css';
+ import downloadCode from './downloadCode.js';
 
  function BlocklyCode({code}) {
+
+  const downloadHandler = (event) => {
+    event.preventDefault();
+    downloadCode(code);
+  };
+
   return (
     <div className='BlocklyCode'>
+      <button onClick={downloadHandler}>DOWNLOAD</button>
       <div className='blockly-current-code'>
         {code}
-        {/* {code.split().map()} */}
       </div>
     </div>
   );
